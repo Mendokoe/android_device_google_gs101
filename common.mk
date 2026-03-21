5ef36df9db16a9612668f907acc2ad1af1053ea4
xioyo <sufiyanmp07@gmail.com>
2026-03-21 12:05:25 +0000

    gs101: Bringup @Lunaris-AOSP

--- common.mk
+++ common.mk
@@ -5,6 +5,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+BUILD_BROKEN_DUP_RULES := true
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
+
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
