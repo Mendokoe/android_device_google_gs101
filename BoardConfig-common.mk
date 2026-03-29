36d6b116aa2116bc201d1549b676c8e60af9687f
Mendokoe <byzel38@gmail.com>
2026-03-29 12:42:21 +0200

    gs101: Bringup @Project-Mist-OS

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -5,6 +5,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+BUILD_BROKEN_DUP_RULES := true
+
 include build/make/target/board/BoardConfigMainlineCommon.mk
 include build/make/target/board/BoardConfigPixelCommon.mk
 
